43099c5ccf72e3953e233733b9b814bfafa63ee5
oueya1479 <oueya1479@naver.com>
2021-04-17 14:50:33 +0900

    Add database connection

--- meosinna/WebContent/index.jsp
+++ meosinna/WebContent/index.jsp
@@ -9,5 +9,6 @@
 </h1>
 <br/>
 <a href="hello-servlet">Hello Servlet</a>
+<a href="asd">안녕</a>
 </body>
 </html>
---
+++ meosinna/src/kosta/controller/MemberServlet.java
@@ -0,0 +1,22 @@
+package kosta.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kosta.dao.order.MemberDAO;
+
+
+@WebServlet("/asd")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		MemberDAO mem = new MemberDAO();
+		mem.member();
+	}
+}
