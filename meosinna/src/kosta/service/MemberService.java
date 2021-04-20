433b20705d0f8ebaca734bbf78dd52a69a6cc13f
Jeonghee <79793074+JeongheeKim219@users.noreply.github.com>
2021-04-20 17:37:54 +0900

    6. 되나보자

--- meosinna/src/kosta/service/MemberService.java
+++ meosinna/src/kosta/service/MemberService.java
@@ -13,9 +13,11 @@ public class MemberService {
 
 		int result = dao.insert(member);
 		if (result == 0) {
-			throw new SQLException("�벑濡앸릺吏� �븡�븯�뒿�땲�떎.");
+			throw new SQLException("등록되지 않았습니다.");
 		}
 		
+	}
+	
 	public Member loginCheck(Member member) throws SQLException{
 		Member dbMember = dao.loginCheck(member);
 		if(dbMember == null) {
